e061069246cebfa55fe264c1ac4677efd414664f
Per Abich <per.abich@gmail.com>
2016-01-27 07:21:00 +0000

    Docker is connected now

--- iam.tf
+++ iam.tf
@@ -1,24 +1,24 @@
 resource "aws_iam_instance_profile" "default" {
   name = "ecsInstanceProfile"
   roles = [
-    "${aws_iam_role.ecsRole.name}"]
+    "${aws_iam_role.ecsRole.name}"
+  ]
 }
-
+resource "aws_iam_role_policy" "ecs" {
+  name = "ecs"
+  policy = "${file("ecsRole_policy.json")}"
+  role = "${aws_iam_role.ecsRole.name}"
+}
 resource "aws_iam_role" "ecsRole" {
   name = "ecsRole"
+
   assume_role_policy = <<EOF
 {
   "Version": "2008-10-17",
   "Statement": [
       {
           "Action": [
-            "sts:AssumeRole",
-            "ecs:DeleteCluster",
-            "ecs:DeregisterContainerInstance",
-            "ecs:ListContainerInstances",
-            "ecs:RegisterContainerInstance",
-            "ecs:SubmitContainerStateChange",
-            "ecs:SubmitTaskStateChange"
+            "sts:AssumeRole"
           ],
           "Principal": {
               "Service": "ec2.amazonaws.com"
