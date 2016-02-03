6c6388f5ea68ea230f43b8234ff9274c9ebcf616
Per Abich <per.abich@gmail.com>
2016-02-03 21:02:39 +0000

    Fixed starting of ecs-agent via puppet

--- cloud-config/dockerServer.pp
+++ cloud-config/dockerServer.pp
@@ -1,28 +1,29 @@
 $region=$::ec2_placement_availability_zone
 
 file { '/etc/sysconfig/docker':
-  ensure  => "file",
-  owner   => "root",
-  group   => "root",
-  mode    => "644",
+  ensure   => "file",
+  owner    => "root",
+  group    => "root",
+  mode     => "644",
   content  => template("/tmp/puppet/docker"),
 }
 
 file { '/etc/ecs/ecs.config':
-  ensure => "file",
+  ensure  => "file",
   owner   => "root",
   group   => "root",
   mode    => "644",
   content => template('/tmp/puppet/ecs.config'),
 }
 
-user {'ec2-user':
+user { 'ec2-user':
   ensure => "present",
-  uid => 500,
-  gid => "ec2-user",
+  uid    => 500,
+  gid    => "ec2-user",
   groups => ["wheel","docker"],
 }
 exec { '/usr/libexec/amazon-ecs-init start':
-   onlyif => '[[ ! $(docker ps |grep ecs-agent) ]]'
+  path   => "/usr/local/bin:/usr/bin:/bin",
+  onlyif => 'test ! $(docker ps |grep ecs-agent)'
 }
 # End node mynode.example.com
